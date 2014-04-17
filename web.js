91a6a174d0aad6f47c9fa9f672925985aac9caf8
Artem Chibeskov <chiart85@gmail.com>
2014-04-17 16:09:56 +0000

    bu1

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Ksu');
+  response.send('Hello Ksu :*');
 });
 
 var port = process.env.PORT || 8080;
